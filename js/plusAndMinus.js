ad452b93d6b19bf901a16541c14cd8bddbe6c314
zameer2020 <zameerbashamirza2020@gmail.com>
2023-01-19 13:03:12 +0530

    Incement and Decrement operations

---
+++ js/plusAndMinus.js
@@ -0,0 +1,9 @@
+let bonusPoints=50;
+bonusPoints=bonusPoints+50;
+console.log(bonusPoints);
+bonusPoints=bonusPoints-75;
+console.log(bonusPoints);
+bonusPoints=bonusPoints+45;
+console.log(bonusPoints);
+
+
